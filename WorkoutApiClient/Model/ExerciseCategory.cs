aa2ad89b5f6e6f7339fb1c63bf32b38210d4f687
Joseph Egglestone <joseph.egglestone@outlook.com>
2019-12-29 20:56:35 +0000

    ExerciseCategory model and json converter

---
+++ WorkoutApiClient/Model/ExerciseCategory.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace WorkoutApiClient.Model
+{
+    public partial class ExerciseCategory
+    {
+        [JsonProperty("count")]
+        public long Count { get; set; }
+
+        [JsonProperty("next")]
+        public object Next { get; set; }
+
+        [JsonProperty("previous")]
+        public object Previous { get; set; }
+
+        [JsonProperty("results")]
+        public List<Result> Results { get; set; }
+    }
+
+    public partial class Result
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+
+    public partial class ExerciseCategory
+    {
+        public static ExerciseCategory FromJson(string json) => JsonConvert.DeserializeObject<ExerciseCategory>(json, Converter.Settings);
+    }
+}
