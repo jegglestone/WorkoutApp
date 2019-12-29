b7baed1086c7b48470b394038f7fd67a448d0804
Joseph Egglestone <joseph.egglestone@outlook.com>
2019-12-29 21:20:58 +0000

    Add serializer and plumbing for an exercise list

---
+++ WorkoutApiClient/Model/Exercises.cs
@@ -0,0 +1,108 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkoutApiClient.Model
+{
+    public partial class Exercises
+    {
+        [JsonProperty("count")]
+        public long Count { get; set; }
+
+        [JsonProperty("next")]
+        public Uri Next { get; set; }
+
+        [JsonProperty("previous")]
+        public object Previous { get; set; }
+
+        [JsonProperty("results")]
+        public List<Exercise> Results { get; set; }
+    }
+
+    public class Exercise
+    {
+        [JsonProperty("id")]
+        public int ExerciseId { get; set; }
+
+        [JsonProperty("license_author")]
+        public string LicenseAuthor { get; set; }
+
+        [JsonProperty("status")]
+        public int Status { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("name")]
+        public string ExerciseName { get; set; }
+
+        [JsonProperty("name_original")]
+        public string NameOriginal { get; set; }
+
+        [JsonProperty("creation_date")]
+        public DateTimeOffset? CreationDate { get; set; }
+
+        [JsonProperty("uuid")]
+        public Guid Uuid { get; set; }
+
+        [JsonProperty("license")]
+        public long License { get; set; }
+
+        [JsonProperty("category")]
+        public long Category { get; set; }
+
+        [JsonProperty("language")]
+        public long Language { get; set; }
+
+        [JsonProperty("muscles")]
+        public List<long> Muscles { get; set; }
+
+        [JsonProperty("muscles_secondary")]
+        public List<long> MusclesSecondary { get; set; }
+
+        [JsonProperty("equipment")]
+        public List<int> Equipment { get; set; }
+    }
+
+    public partial class Exercises
+    {
+        public static Exercises FromJson(string json) => JsonConvert.DeserializeObject<Exercises>(json, Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this Exercises self) => JsonConvert.SerializeObject(self, Converter.Settings);
+    }
+
+    //internal class ParseStringConverter : JsonConverter
+    //{
+    //    public override bool CanConvert(Type t) => t == typeof(long) || t == typeof(long?);
+
+    //    public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+    //    {
+    //        if (reader.TokenType == JsonToken.Null) return null;
+    //        var value = serializer.Deserialize<string>(reader);
+    //        long l;
+    //        if (Int64.TryParse(value, out l))
+    //        {
+    //            return l;
+    //        }
+    //        throw new Exception("Cannot unmarshal type long");
+    //    }
+
+    //    public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+    //    {
+    //        if (untypedValue == null)
+    //        {
+    //            serializer.Serialize(writer, null);
+    //            return;
+    //        }
+    //        var value = (long)untypedValue;
+    //        serializer.Serialize(writer, value.ToString());
+    //        return;
+    //    }
+
+    //    public static readonly ParseStringConverter Singleton = new ParseStringConverter();
+    //}
+}
---
+++ WorkoutApp/Controllers/ExercisesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WorkoutApp.Controllers
+{
+    public class ExercisesController : Controller
+    {
+        public IActionResult Index(int exerciseCategoryId)
+        {
+            // https://wger.de/api/v2/exercise/?format=json&category=8
+
+            // 1. build the url above with the passed in category id
+
+            // 2. get the json using the WorkoutApiHelper class
+
+            // 3. deserialize it using Exercises.FromJson() method that I have created
+
+
+
+            
+            return View(); //return with a view model - call it ExercisesViewModel
+        }
+    }
+}
