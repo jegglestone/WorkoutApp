24be2b39bf768a185ffd4b390245013f4996bb01
peter <TangZeren@outlook.com>
2020-02-10 22:14:09 +1100

    Finish part of inserting images with bugs

--- WorkoutApp/Controllers/CommentsController.cs
+++ WorkoutApp/Controllers/CommentsController.cs
@@ -19,6 +19,21 @@ namespace WorkoutApp.Controllers
             var commentViewModel = new CommentViewModel { CommentsList = comments };
 
             return View(commentViewModel);
+         
+        }
+
+        public IActionResult Image(int exerciseId)
+        {
+            string imageUrl
+                = $"{Constants.BaseWorkoutApiUrl}/exerciseimage/?format=json&language=2&exercise=" + exerciseId;
+
+            string jsonReponse = WorkoutApiHelper.GetJsonStringFromApi(imageUrl);
+
+            ExerciseImage images = ExerciseImage.FromJson(jsonReponse);
+
+            var commentViewModel = new CommentViewModel { ImageList = images };
+
+            return View(commentViewModel);
         }
     }
 }
--- WorkoutApp/Models/CommentViewModel.cs
+++ WorkoutApp/Models/CommentViewModel.cs
@@ -4,5 +4,6 @@ namespace WorkoutApp.Models
     public class CommentViewModel
     {
         public ExerciseComments CommentsList { get; set; }
+        public ExerciseImage ImageList { get; set; }
     }
 }
